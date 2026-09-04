992a76eea059943262cea6544e9b22298ee2c43c
lornechen1-hue <lornechen1@gmail.com>
2026-09-04 20:14:18 +0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    return new Response("Hello! My site is connected to GitHub! 🎉", {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
